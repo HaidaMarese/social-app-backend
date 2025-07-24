2fde01630c33319aaba99889941fd088c5a904f1
Haida Makouangou <hmakouan06@gmail.com>
2025-07-24 10:19:04 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import db from "./config/connection.js";
+import usersRouter from './routes/users.js';
+import postsRouter from './routes/posts.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({origin: "http://localhost:5173"}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
